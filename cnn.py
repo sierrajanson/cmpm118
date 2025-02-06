77eff1160d8ae4fc9b3e08525ab17eac9bfa244c
Ruthwika Gajjala <rgajjala@ucsc.edu>
2025-02-06 15:33:00 -0800

    fixed smt

--- cnn.py
+++ cnn.py
@@ -8,6 +8,7 @@ import torchvision.transforms as transforms
 from torch import nn
 
 import snntorch as snn
+from snntorch import utils
 import tonic 
 import snntorch.functional as F
 import tonic.transforms as transforms
@@ -18,19 +19,31 @@ EPOCHS = 3
 BETA = 0.99
 
 # load data
-transform = transforms.ToTensor()
-trainset = tonic.datasets.NMNIST(root='./data', train=True, download=True, transform=transform) # change this to tonic (for NMNIST)
-testset = tonic.datasets.NMNIST(root='./data', train=False, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True) # need to import both train and test
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+# Define the sensor size for NMNIST
+sensor_size = tonic.datasets.NMNIST.sensor_size
+
+# Create a transform pipeline
+transform = transforms.Compose([
+    transforms.Denoise(filter_time=10000),
+    transforms.ToFrame(sensor_size=sensor_size, time_window=10000)
+])
+
+# Load the datasets
+trainset = tonic.datasets.NMNIST(save_to='./data', train=True, transform=transform)
+testset = tonic.datasets.NMNIST(save_to='./data', train=False, transform=transform)
+
+# Create data loaders
+batch_size = 64
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, collate_fn=tonic.collation.PadTensors(batch_first=True))
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, collate_fn=tonic.collation.PadTensors(batch_first=True))
 
 # define convolutional layers
 # in_channels --> initially # of colors (1 for grayscale, 3 for RGB)
 # out_channels --> how many features derived from conv layer, to be passed as input to next layer
 # kernel (filter) size (amplifier square multiplied w/ OG image)--> 3 is standard
-conv1 = torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1) 
-conv2 = torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
-fc = torch.nn.Linear(32 * 7 * 7, 10)
+conv1 = nn.Conv2d(2, 16, kernel_size=3, padding=1)
+conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+fc = nn.Linear(32 * 7 * 7, 10)
 
 # optimizer
 optimizer = torch.optim.Adam(list(conv1.parameters()) + list(conv2.parameters()) + list(fc.parameters()),  lr=0.001)
@@ -42,27 +55,25 @@ num_hidden = 1000
 num_outputs = 10
 beta = 0.99
 
-# fc1 = nn.Linear(num_inputs, num_hidden)
-# lif1 = snn.Leaky(beta=beta)
-# fc2 = nn.Linear(num_hidden, num_outputs)
-# lif2 = snn.Leaky(beta=beta)
+# define SNN layers
+snn1 = snn.Leaky(beta=BETA, spike_grad=surrogate.fast_sigmoid(slope=25))
+snn2 = snn.Leaky(beta=BETA, spike_grad=surrogate.fast_sigmoid(slope=25))
+snn3 = snn.Leaky(beta=BETA, spike_grad=surrogate.fast_sigmoid(slope=25), output=True)
 
-# mem1 = lif1.init_leaky()
-# mem2 = lif2.init_leaky()
-
-# mem2_rec = []
-# spk1_rec = []
-# spk2_rec = []
-
-net = nn.Sequential(conv1, nn.MaxPool2d(2), snn.Leaky(beta=beta, spike_grad=gradient), conv2, nn.MaxPool2d(2), snn.Leaky(beta=beta, spike_grad=gradient), nn.Flatten(), fc1, snn.Leaky(x,beta=beta, spike_grad=gradient, output=True))
+net = nn.Sequential(
+    conv1, nn.MaxPool2d(2), snn1,
+    conv2, nn.MaxPool2d(2), snn2,
+    nn.Flatten(), fc, snn3
+)
 
 def forward(net, x):
     spk_rec = []
-    utils.reset(net)
-    for step in range(x.size(0)):
-        spk_out, mem_out = net(data[step])
+    utils.reset(net)  
+    for step in range(x.size(0)): 
+        spk_out, mem_out = net(x[step])
         spk_rec.append(spk_out)
     return torch.stack(spk_rec)
+
 # def forward(x):
 #     x = F.relu(conv1(x))
 #     x = F.max_pool2d(x, 2, 2) # add leaky snn here
@@ -80,7 +91,7 @@ def forward(net, x):
 for epoch in range(EPOCHS):
     for images, labels in trainloader:
         optimizer.zero_grad()
-        x = forward(images) # forward pass
+        x = forward(net, images) # forward pass
         # loss = F.cross_entropy(x, labels)
         loss = F.mse_count_loss(x, labels)
         loss.backward()
